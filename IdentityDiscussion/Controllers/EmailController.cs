dc0684eaa159615175aa31cec4e36adb5f94265e
yaswanth <ya8wanth@gmail.com>
2023-04-25 11:47:34 +0530

    Invite tab added

---
+++ IdentityDiscussion/Controllers/EmailController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace IdentityDiscussion.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult SendEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(string address,string subject,string body)
+        {
+            Console.WriteLine(address);
+            Console.WriteLine(subject);
+            Console.WriteLine(body);
+            return View();
+        }
+    }
+}
